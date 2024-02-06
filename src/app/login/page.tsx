ca2e3d689e72f41b5e46a2f12be76ade4fc9587d
TerryEHLee <terryehlee@gmail.com>
2024-02-06 22:59:51 +0900

    feat/login-makingFrame
    Made big frame which contains login btn and the placeholder for initial and pw.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import Login from "../../../components/auth/Login";
+
 const login = () => {
   return (
     <>
       <div className="text-2xl pb-16 text-gray-900 font-semibold">로그인</div>
+      <div>
+        <Login />
+      </div>
     </>
   );
 };
