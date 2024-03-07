d538b642d9767b9dcf712209b05740d135db8bfa
TerryEHLee <terryehlee@gmail.com>
2024-03-07 16:21:47 +0900

    feat/login
    making frame: sched/ui
    validation checking: zod, react-hook-form
    
    서버와 연동하여, 로그인 하는 과정 추가 구현예정

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -28,5 +28,83 @@ import React, { useState } from "react";
 type LoginInput = z.infer<typeof loginSchema>;
 
 export function Login() {
-  return <>Login</>;
+  const form = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      name: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: LoginInput) {
+    form.trigger(["name", "password"]);
+
+    const nameState = form.getFieldState("name");
+    const passwordState = form.getFieldState("password");
+
+    if (
+      nameState.isDirty &&
+      !nameState.invalid &&
+      passwordState.isDirty &&
+      !passwordState.invalid
+    ) {
+      alert(JSON.stringify(data, null, 4));
+    }
+  }
+
+  return (
+    <>
+      <Card className="w-[380px]">
+        <CardHeader>
+          <CardTitle>계정을 생성합니다</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="relative space-y-3 overflow-x-hidden"
+            >
+              <div className="grid w-full items-center gap-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>이름</FormLabel>
+                      <FormControl>
+                        <Input placeholder="이름을 입력하세요" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>비밀번호</FormLabel>
+                      <FormControl>
+                        <Input placeholder="비밀번호를 입력하세요" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex items-center">
+          <Button
+            variant="outline"
+            type="submit"
+            onClick={() => onSubmit(form.getValues())}
+          >
+            로그인
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
