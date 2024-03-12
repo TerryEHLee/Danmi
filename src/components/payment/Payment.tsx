8c383fbebac5b401b1a64032dea8d848130567fe
TerryEHLee <terryehlee@gmail.com>
2024-03-12 23:47:37 +0900

    feat/frame
    frame 완성
    쿠폰과 포인트 중 사용을 원하는 것을 선택한 후에
    보유 쿠폰 혹은 보유 포인트를 확인하도록 로직 업데이트 예정
    
    json-server로 로그인 기능을 구현한 후에
    해당 회원의 보유 쿠폰/포인트 정보를 불러와 연동예정정

--- src/components/payment/Payment.tsx
+++ src/components/payment/Payment.tsx
@@ -49,9 +49,35 @@ export function Payment() {
   const [selectedLessonType, setSelectedLessonType] = useState<string | null>(
     null,
   );
+  const [selectedPrice, setSelectedPrice] = useState<string | null>(null);
+  const [selectedCoupon, setSelectedCoupon] = useState(null);
+  const [isCouponSelected, setIsCouponSelected] = useState(false);
+  const [isPointInputDisabled, setIsPointInputDisabled] = useState(false);
 
   const handleLessonTypeClick = (lessonType: string) => {
     setSelectedLessonType(lessonType);
+  };
+
+  const handlePriceButtonClick = (price: string) => {
+    setSelectedPrice(price);
+  };
+
+  const toggleCouponSelection = (coupon) => {
+    setSelectedCoupon(coupon);
+    setIsCouponSelected(true);
+    setIsPointInputDisabled(true);
+  };
+
+  const handlePointInputChange = (e) => {
+    setSelectedCoupon(null);
+    setIsCouponSelected(false);
+    setIsPointInputDisabled(false);
+  };
+
+  const handleUseAllPoints = () => {
+    setSelectedCoupon(null);
+    setIsCouponSelected(false);
+    setIsPointInputDisabled(false);
   };
 
   function onSubmit(data: PaymentInput) {
@@ -71,6 +97,9 @@ export function Payment() {
                     e.preventDefault();
                     handleLessonTypeClick("1:1");
                   }}
+                  className={
+                    selectedLessonType === "1:1" ? "bg-danmi text-white" : ""
+                  }
                 >
                   1:1 개인레슨
                 </Button>
@@ -80,6 +109,9 @@ export function Payment() {
                     e.preventDefault();
                     handleLessonTypeClick("2:1");
                   }}
+                  className={
+                    selectedLessonType === "2:1" ? "bg-danmi text-white" : ""
+                  }
                 >
                   2:1 듀엣레슨
                 </Button>
@@ -89,6 +121,9 @@ export function Payment() {
                     e.preventDefault();
                     handleLessonTypeClick("4:1");
                   }}
+                  className={
+                    selectedLessonType === "4:1" ? "bg-danmi text-white" : ""
+                  }
                 >
                   4:1 그룹레슨
                 </Button>
@@ -96,17 +131,53 @@ export function Payment() {
 
                 {selectedLessonType === "1:1" && (
                   <div>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("704000원");
+                      }}
+                      className={
+                        selectedPrice === "704000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       10회 | 2개월 <br />
                       회당 70400원 <br />
                       704000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("1320000원");
+                      }}
+                      className={
+                        selectedPrice === "1320000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       20회 | 3개월 <br />
                       회당 66000원 <br />
                       1320000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("1770000원");
+                      }}
+                      className={
+                        selectedPrice === "1770000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       30회 | 4개월 <br />
                       회당 59000원 <br />
                       1770000원
@@ -116,17 +187,53 @@ export function Payment() {
 
                 {selectedLessonType === "2:1" && (
                   <div>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("470000원");
+                      }}
+                      className={
+                        selectedPrice === "470000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       10회 | 2개월 <br />
                       회당 47000원 <br />
                       470000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("830000원");
+                      }}
+                      className={
+                        selectedPrice === "830000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       20회 | 3개월 <br />
                       회당 41500원 <br />
                       830000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("1080000원");
+                      }}
+                      className={
+                        selectedPrice === "1080000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       30회 | 4개월 <br />
                       회당 36000원 <br />
                       1080000원
@@ -136,22 +243,70 @@ export function Payment() {
 
                 {selectedLessonType === "4:1" && (
                   <div>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("212000원");
+                      }}
+                      className={
+                        selectedPrice === "212000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       8회 | 1개월 <br />
                       회당 26500원 <br />
                       212000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("368000원");
+                      }}
+                      className={
+                        selectedPrice === "368000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       16회 | 2개월 <br />
                       회당 23000원 <br />
                       368000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("480000원");
+                      }}
+                      className={
+                        selectedPrice === "480000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       24회 | 3개월 <br />
                       회당 20000원 <br />
                       480000원
                     </Button>
-                    <Button variant="outline" size="lg">
+                    <Button
+                      variant="outline"
+                      size="lg"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handlePriceButtonClick("864000원");
+                      }}
+                      className={
+                        selectedPrice === "864000원"
+                          ? "bg-danmi text-white"
+                          : ""
+                      }
+                    >
                       48회 | 6개월 <br />
                       회당 18000원 <br />
                       864000원
@@ -170,20 +325,38 @@ export function Payment() {
                 <CardDescription>
                   <div className="flex justify-between mt-2">
                     <div>수업가격</div>
-                    <div className="mr-4">250000</div>
+                    <div className="mr-4">{selectedPrice || "0원"}</div>
                   </div>
                   <div className="flex justify-between">
                     <div>쿠폰할인</div>
-                    <div className="mr-4">250000</div>
+                    <div className="mr-4">
+                      {selectedPrice
+                        ? "-" +
+                          (
+                            parseInt(selectedPrice.replace("원", "")) * 0.1
+                          ).toLocaleString()
+                        : "0"}
+                      원
+                    </div>
                   </div>
                   <div className="flex justify-between">
                     <div>포인트 사용</div>
-                    <div className="mr-4">250000</div>
+                    <div className="mr-4">
+                      {"-" + (form.getValues("point") || "0")}원
+                    </div>
                   </div>
                   <br />
                   <div className="flex justify-between">
                     <strong>최종 결제금액</strong>
-                    <strong className="mr-4">250000</strong>
+                    <strong className="mr-4">
+                      {selectedPrice
+                        ? (
+                            parseInt(selectedPrice.replace("원", "")) -
+                            parseInt(selectedPrice.replace("원", "")) * 0.1 -
+                            parseInt(form.getValues("point") || 0)
+                          ).toLocaleString() + "원"
+                        : "0원"}
+                    </strong>
                   </div>
                 </CardDescription>
               </CardHeader>
@@ -195,7 +368,10 @@ export function Payment() {
         <div>
           <Card className="w-[700px]">
             <CardHeader>
-              <CardTitle>쿠폰 & 포인트</CardTitle>
+              <CardTitle>쿠폰 | 포인트</CardTitle>
+              <CardDescription>
+                쿠폰과 포인트는 동시에 사용할 수 없습니다
+              </CardDescription>
             </CardHeader>
             <CardContent>
               <Form {...form}>
@@ -209,14 +385,17 @@ export function Payment() {
                     render={({ field }) => (
                       <FormItem>
                         <Select
-                          onValueChange={field.onChange}
+                          onValueChange={(value) => {
+                            field.onChange(value);
+                            toggleCouponSelection(value);
+                          }}
                           defaultValue={field.value}
+                          disabled={isPointInputDisabled}
                         >
-                          <FormControl>
-                            <SelectTrigger>
-                              <SelectValue placeholder="쿠폰 사용하기" />
-                            </SelectTrigger>
-                          </FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="쿠폰 사용하기" />
+                          </SelectTrigger>
+
                           <SelectContent>
                             <SelectItem value="coupon10">
                               10% 할인쿠폰
@@ -238,14 +417,20 @@ export function Payment() {
                         <FormItem>
                           <FormLabel>사용할 포인트를 입력하세요</FormLabel>
                           <FormControl>
-                            <Input placeholder="보유포인트: 5000" {...field} />
+                            <Input
+                              placeholder="보유포인트: 5000"
+                              {...field}
+                              disabled={isCouponSelected}
+                              onChange={handlePointInputChange}
+                            />
                           </FormControl>
                           <FormMessage />
                         </FormItem>
                       )}
                     />
                     <CardFooter className="flex justify-between">
-                      <Button variant="outline">전액사용</Button>
+                      <Button variant="outline" onClick={handleUseAllPoints}>
+                        전액사용</Button>
                     </CardFooter>
                   </div>
                 </form>
@@ -260,21 +445,7 @@ export function Payment() {
                 <CardTitle>결제수단</CardTitle>
                 <CardDescription>
                   <div className="flex justify-between mt-2">
-                    <div>수업가격</div>
-                    <div className="mr-4">250000</div>
-                  </div>
-                  <div className="flex justify-between">
-                    <div>쿠폰할인</div>
-                    <div className="mr-4">250000</div>
-                  </div>
-                  <div className="flex justify-between">
-                    <div>포인트 사용</div>
-                    <div className="mr-4">250000</div>
-                  </div>
-                  <br />
-                  <div className="flex justify-between">
-                    <strong>최종 결제금액</strong>
-                    <strong className="mr-4">250000</strong>
+                    <div>결제수단업데이트예정</div>
                   </div>
                 </CardDescription>
               </CardHeader>
